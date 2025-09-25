d75d724b5400a342e0daba036e255422abf7d1b2
Shashank Nirmale <nirmale.shahshank@gmail.com>
2025-09-25 21:44:01 +0530

    ok

--- src/Java_ATB14X/Lab001_HelloWorld.java
+++ src/Java_ATB14X/Lab001_HelloWorld.java
@@ -1,4 +1,7 @@
 package Java_ATB14X;
 
 public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
---
+++ src/Java_ATB14X/Lab002_Cityname.java
@@ -0,0 +1,12 @@
+package Java_ATB14X;
+
+import javax.print.DocFlavor;
+
+public class Lab002_Cityname {
+    public static void main(String[] args) {
+        System.out.println("MALAD");
+        System.out.println("Mumbai");
+
+    }
+    }
+
