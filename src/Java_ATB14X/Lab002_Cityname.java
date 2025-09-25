d36966a6420df5d331486d50c21700d7fe22af8b
Shashank Nirmale <nirmale.shahshank@gmail.com>
2025-09-25 21:47:34 +0530

    ok

--- src/Java_ATB14X/Lab002_Cityname.java
+++ src/Java_ATB14X/Lab002_Cityname.java
@@ -6,6 +6,7 @@ public class Lab002_Cityname {
     public static void main(String[] args) {
         System.out.println("MALAD");
         System.out.println("Mumbai");
+        System.out.println("Manmad");
 
     }
     }
