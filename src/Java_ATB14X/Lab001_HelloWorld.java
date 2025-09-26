53a332790fd167a5be81166fe19697dcc9da3c34
Shashank Nirmale <nirmale.shahshank@gmail.com>
2025-09-26 08:12:56 +0530

    ok....Push into GIT

--- src/Java_ATB14X/Lab001_HelloWorld.java
+++ src/Java_ATB14X/Lab001_HelloWorld.java
@@ -2,6 +2,7 @@ package Java_ATB14X;
 
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
     }
 }
---
+++ src/Java_ATB14X/Lab003_TestgitPush.java
@@ -0,0 +1,7 @@
+package Java_ATB14X;
+
+public class Lab003_TestgitPush {
+    public static void main(String[] args) {
+        System.out.println("Code Push into GIT");
+    }
+}
