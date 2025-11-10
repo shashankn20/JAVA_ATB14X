9462a82b56380c8985251afd42c3b64b74c15a87
Shashank Nirmale <nirmale.shahshank@gmail.com>
2025-11-10 17:48:19 +0530

    ok....Java Variables

---
+++ src/Ex001_Java_Basics/Lab014_Variables.java
@@ -0,0 +1,11 @@
+package Ex001_Java_Basics;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
---
+++ src/Ex001_Java_Basics/Lab015_Variables.java
@@ -0,0 +1,12 @@
+package Ex001_Java_Basics;
+
+public class Lab015_Variables {
+    public static void main(String[] args) {
+
+        //data_type varible_name   =    variable_value (i.e Identifier) //
+        //byte age = 65 ; byte is data type, age is variable name and 65 is variable value
+        //System.out.println(age);//
+        byte age_shashank= 34;
+        System.out.println(age_shashank);
+    }
+}
