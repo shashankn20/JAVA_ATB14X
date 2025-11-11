acce9582e838d80525328b5dd8f10d28c222e63a
Shashank Nirmale <nirmale.shahshank@gmail.com>
2025-11-11 09:11:08 +0530

    ok....Numeric_DataTypes

---
+++ src/Ex001_Java_Basics/Lab020_Numeric_DataTypes.java
@@ -0,0 +1,19 @@
+package Ex001_Java_Basics;
+
+public class Lab020_Numeric_DataTypes {
+    public static void main(String[] args) {
+        byte b =10;
+        int i = 25;
+        short  s = 15;
+        char c= 'A';
+
+        long  phone= 89863533434l;
+        long  phone1= 86767543543l;
+
+        float f1= 1.55f;
+        float f2= 2.985988f;
+
+        double d1= 3.25545454;
+
+    }
+}
